def27d35e2b0a82149e01a570833cc0c530021a1
Nobuyoshi Shimmen <nobuyoshi.shimmen@gmail.com>
2020-10-25 21:35:26 +0900

    devのときはtest用のadid

--- lib/ad_manager.dart
+++ lib/ad_manager.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:hosheee/main.dart';
+
 class AdManager {
 
   static String get appId {
@@ -13,12 +15,13 @@ class AdManager {
   }
 
   static String get bannerAdUnitId {
+    if (EnvironmentConfig.BUILD_ENV == "dev") {
+      return "ca-app-pub-3940256099942544/2934735716";
+    }
     if (Platform.isAndroid) {
-      // return "ca-app-pub-1121620757092541/2360385558";
-      return "ca-app-pub-3940256099942544/2934735716";
+      return "ca-app-pub-1121620757092541/2360385558";
     } else if (Platform.isIOS) {
-      // return "ca-app-pub-1121620757092541/6926316526";
-      return "ca-app-pub-3940256099942544/2934735716";
+      return "ca-app-pub-1121620757092541/6926316526";
     } else {
       throw new UnsupportedError("Unsupported platform");
     }
